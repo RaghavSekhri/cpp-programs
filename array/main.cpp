033718213e27528a0d453aae067dcc6ef500b9d6
Raghav Sekhri <raghavsekhri2406@gmail.com>
2020-01-26 16:08:55 +0530

    Update main.cpp

--- array/main.cpp
+++ array/main.cpp
@@ -1,18 +1,13 @@
 #include <iostream>
-
 using namespace std;
 
 int main()
-
+{
    int *a=new int[5];//dynamically allocated the memory
    int i;
    for(i=0;i<5;i++)
-   {
        cin>>a[i];
-   }
    for(i=0;i<5;i++)
-   {
        cout<<a[i]<<"\n";
-   }
     return 0;
 }
